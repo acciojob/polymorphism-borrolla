acdbb6f5974bad5c8e30bb8fb2ded7b7980c51af
shravan kumar <sravan3251@gmail.com>
2025-01-13 23:20:40 +0530

    Polymorphism assignment solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,37 @@
 package com.driver;
-
 public class Main {
+   //Task 1:
+    // * create a class Product inside Main class
+    public static class Product{
+        //create a method of following defination
+        public int product(int x, int y){
+             return x * y;
+        }
+
+        //Task 4:
+       // * create a Overloaded method product of following defination
+       // * public int product(int x, int y, int z) {}
+        public int product(int x, int y,int z)
+        {
+            return x * y * z;
+        }
+
+        ///  Task 5:
+        ///  * create a Overloaded method product of following defination
+        ///  * public double product(double x, double y) {}
+       public double product(double x, double y) {
+           return x * y;
+
+       }
+   }
+    public static void main(String[] args) {
+        //create object of Product in Main function called p
+        Product p = new Product();
+
+        // Call the methods and print the results
+        System.out.println("Product of 2 and 3 (int): " + p.product(2,3));
+        System.out.println("Product of 2, 3 and 4: " + p.product(2,3,4));
+        System.out.println("product of 2.5 and  3.5: " + p.product(2.5,3.5));
+    }
 
 }
